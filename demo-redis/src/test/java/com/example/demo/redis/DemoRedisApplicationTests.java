15b210d4db4ceb52b3a3e4009bb1543e9d0ba202
hrd <1115040204@qq.com>
2022-06-27 00:14:11 +0800

    redisutil

--- demo-redis/src/test/java/com/example/demo/redis/DemoRedisApplicationTests.java
+++ demo-redis/src/test/java/com/example/demo/redis/DemoRedisApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo.redis;
 
-import org.junit.jupiter.api.Test;
+import com.example.demo.redis.util.RedisUtil;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoRedisApplicationTests {
 
+    @Autowired
+    private RedisUtil redisUtil;
     @Test
     void contextLoads() {
+
     }
 
 }
